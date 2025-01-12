1bc1bbd425a431a6acc5ce939e3e8b38cbd79aa2
Luo Jian <crackedpoly@outlook.com>
2025-01-12 16:47:41 +0800

    chore: add an example in the README
    Signed-off-by: Luo Jian <crackedpoly@outlook.com>

--- README.md
+++ README.md
@@ -7,12 +7,89 @@ Inverse Model Transformation in Rust
 
 ## Introduction
 
+Rapimt is a data plane verification framework written in Rust.
+
+## Features
+
+- currently supported match fields: `src_ip`, `dst_ip`, `src_port`, `dst_port`, `vlan`
+- predicate engines: `Ruddy`
+- verification functions: TODO
+
+## Example
+
+How many equivalent classes are there in a network? Compute it by Inverse
+Models!
+
+```rust
+let engine = RuddyPredicateEngine::init(100, 10);
+let loader = DefaultInstLoader {};
+let devs = vec![
+    "bbra_rtr", "bbrb_rtr", "boza_rtr", "bozb_rtr", "coza_rtr", "cozb_rtr", "goza_rtr",
+    "gozb_rtr", "poza_rtr", "pozb_rtr", "roza_rtr", "rozb_rtr", "soza_rtr", "sozb_rtr",
+    "yoza_rtr", "yozb_rtr",
+];
+
+// 1. Load topology (port information)
+let mut codexs = FxHashMap::default();
+for dev in devs.iter() {
+    let spec_cont =
+        std::fs::read_to_string(format!("examples/stanford/spec/{}.spec", dev)).unwrap();
+    let codex = InstanceLoader::load(&loader, &spec_cont).unwrap();
+    codexs.insert(dev, codex);
+}
+
+// 2. Create rule monitors
+let mut monitors = FxHashMap::default();
+for dev in devs.iter() {
+    monitors.insert(dev, FastRuleMonitor::<_, _, TPTRuleStore<_, _>>::new(&engine));
+}
+
+// Global inverse model
+// We choose FxHashMap to store the network-wide inverse model and Vec to store the actions.
+let mut im: InverseModel<_, _, _, FxHashMap<Vec<_>, _>> = InverseModel::default();
+// Incremental updates
+let mut im_updates = FxHashMap::default();
+
+// 3. Load fibs and get incremental updatek of each device
+for d in devs.iter() {
+    let fib_cont = std::fs::read_to_string(format!("examples/stanford/fib/{}.fib", d)).unwrap();
+    // Load, parse and encode fib rules
+    let fibs = codexs[d].load(&engine, &fib_cont).unwrap().1;
+    // Feed fibs to the monitor and get the incremental update in the form of inverse model
+    // NOTICE:
+    //   1. we use FxHashMap to store the inverse model entries.
+    //   2. we use usize to represent an action in the device, alternatively, we can use
+    //      TypedAction here.
+    let im_update = monitors
+        .get_mut(d)
+        .unwrap()
+        .insert::<_, _, FxHashMap<usize, _>>(fibs);
+    // .insert::<_, _, FxHashMap<TypedAction, _>>(fibs);
+    im_updates.insert(d, im_update);
+}
+
+// 4. Merge incremental updates into one big network model
+for (d, im_update) in im_updates {
+    let idx = devs.iter().position(|x| x == d).unwrap();
+    // expand usize to Vec<usize>
+    let im_update = InverseModel::from(im_update);
+    // resize local inverse model to network-wide inverse model
+    let im_update = InverseModel::resize(im_update, devs.len(), idx);
+    // merge the inverse model
+    im <<= im_update;
+}
+
+// 5. Check the number of equivalent classes in the network-wide
+// The number of equivalent classes in this stanford dataset is 155
+assert_eq!(im.len(), 155)
+```
+
 ## Installation
 
 ## TODO List
 
 - [x] Use patricia tree to store rules in a device
-- [ ] Optimize the inverse model resizing
+- [x] Optimize the inverse model resizing
 - [ ] Benchmark the performance in larger datasets
 - [ ] Implement more verification modules, such as the paper "Modular DPV for
   Compositional Networks"
--- crates/rapimt_im/examples/stanford/stanford.rs
+++ crates/rapimt_im/examples/stanford/stanford.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::SystemTime};
+use std::time::SystemTime;
 
 use fxhash::FxHashMap;
 use rapimt_core::prelude::RuddyPredicateEngine;
@@ -8,44 +8,43 @@ use rapimt_io::prelude::{DefaultInstLoader, FibLoader, InstanceLoader, PortInfoB
 fn main() {
     let engine = RuddyPredicateEngine::init(100, 10);
     let loader = DefaultInstLoader {};
+    let devs = vec![
+        "bbra_rtr", "bbrb_rtr", "boza_rtr", "bozb_rtr", "coza_rtr", "cozb_rtr", "goza_rtr",
+        "gozb_rtr", "poza_rtr", "pozb_rtr", "roza_rtr", "rozb_rtr", "soza_rtr", "sozb_rtr",
+        "yoza_rtr", "yozb_rtr",
+    ];
 
-    // get device names of dataset
-    let mut devs = vec![];
-    for entry in std::fs::read_dir("examples/stanford/fib").unwrap() {
-        let path = entry.unwrap().path();
-        let name = path.file_stem().unwrap().to_str().unwrap();
-        devs.push(name.to_string());
-    }
-
-    // load topology (port information)
-    let mut codexs = FxHashMap::<String, PortInfoBase>::default();
+    // 1. Load topology (port information)
+    let mut codexs = FxHashMap::default();
     for dev in devs.iter() {
         let spec_cont =
             std::fs::read_to_string(format!("examples/stanford/spec/{}.spec", dev)).unwrap();
         let codex = InstanceLoader::load(&loader, &spec_cont).unwrap();
-        codexs.insert(dev.clone(), codex);
+        codexs.insert(dev, codex);
     }
 
-    // create monitors
-    let mut monitors = FxHashMap::<String, FastRuleMonitor<_, _, TPTRuleStore<_, _>>>::default();
-    for dev in codexs.keys() {
-        monitors.insert(dev.clone(), FastRuleMonitor::new(&engine));
+    // 2. Create rule monitors
+    let mut monitors = FxHashMap::default();
+    for dev in devs.iter() {
+        monitors.insert(dev, FastRuleMonitor::<_, _, TPTRuleStore<_, _>>::new(&engine));
     }
 
     let mut mr1_timer = 0u128;
     let mut r2_timer = 0u128;
 
-    // we choose FxHashMap to store the network-wide inverse model and Vec to store the actions.
+    // Global inverse model
+    // We choose FxHashMap to store the network-wide inverse model and Vec to store the actions.
     let mut im: InverseModel<_, _, _, FxHashMap<Vec<_>, _>> = InverseModel::default();
-    let mut im_updates = HashMap::new();
+    // Incremental updates
+    let mut im_updates = FxHashMap::default();
 
-    // load fibs and get inverse model of each device
-    for d in codexs.keys() {
+    // 3. Load fibs and get incremental updatek of each device
+    for d in devs.iter() {
         let fib_cont = std::fs::read_to_string(format!("examples/stanford/fib/{}.fib", d)).unwrap();
-        // load, parse and encode fib rules
+        // Load, parse and encode fib rules
         let fibs = codexs[d].load(&engine, &fib_cont).unwrap().1;
         let _timer = SystemTime::now();
-        // feed fibs to the monitor and get the incremental update in the form of inverse model
+        // Feed fibs to the monitor and get the incremental update in the form of inverse model
         // NOTICE:
         //   1. we use FxHashMap to store the inverse model entries.
         //   2. we use usize to represent an action in the device, alternatively, we can use
@@ -54,25 +53,27 @@ fn main() {
             .get_mut(d)
             .unwrap()
             .insert::<_, _, FxHashMap<usize, _>>(fibs);
-            // .insert::<_, _, FxHashMap<TypedAction, _>>(fibs);
+        // .insert::<_, _, FxHashMap<TypedAction, _>>(fibs);
         mr1_timer += _timer.elapsed().unwrap().as_nanos();
-        im_updates.insert(d.clone(), im_update);
+        im_updates.insert(d, im_update);
     }
 
-    // merge inverse models into one big network model
+    // 4. Merge incremental updates into one big network model
     for (d, im_update) in im_updates {
-        let idx = devs.iter().position(|x| *x == d).unwrap();
+        let idx = devs.iter().position(|x| x == d).unwrap();
         // expand usize to Vec<usize>
         let im_update = InverseModel::from(im_update);
         // resize local inverse model to network-wide inverse model
         let im_update = InverseModel::resize(im_update, devs.len(), idx);
         let _timer = SystemTime::now();
+        // merge the inverse model
         im <<= im_update;
         r2_timer += _timer.elapsed().unwrap().as_nanos();
     }
     println!("Monitor refresh time: {} us", mr1_timer / 1000);
     println!("Inverse model << time: {} us", r2_timer / 1000);
 
-    // the number of equivalent classes in this stanford dataset is 155
+    // 5. Check the number of equivalent classes in the network-wide
+    // The number of equivalent classes in this stanford dataset is 155
     assert_eq!(im.len(), 155)
 }
